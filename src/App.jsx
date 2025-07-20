2c83006d81a7a59a425be30373ca07f34bf5b74c
T-supernatural <tinuademichael@gmail.com>
2025-07-20 22:36:23 +0100

    commit changes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-5xl text-teal-500 bg-amber-300 capitalize">
+      My name is tinuade michael
+    </div>
+  )
+}
+
+export default App
